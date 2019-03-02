003d16347543806a6bc8b3512ffdbfc674224d13
benelliraul <47056733+benelliraul@users.noreply.github.com>
2019-03-02 15:14:56 -0300

    Update DataBase.py

--- BackEnd/database/DataBase.py
+++ BackEnd/database/DataBase.py
@@ -16,6 +16,18 @@ class Basedatos:
         self.db = sqlite3.connect("base_datos.db")
         self.cursor = self.db.cursor()
 
+    def commit(self):
+        """Valida las transacciones realizadas en la base de datos, se ejecuta luego de 
+        realizar transacciones que modifiquen la base de datos"""
+
+        self.db.commit()
+
+    def cerrar_conexion(self):
+        """Cierra el cursor y la conexión a la base de datos"""
+
+        self.cursor.close()
+        self.db.close()
+
     def crear_tabla(self):
         """Crea la tabla "tiendas" si no existe, con seis  columnas, la primera
         reservada para clave primaria que se añade automaticamete """
@@ -29,11 +41,8 @@ class Basedatos:
 									   ruta_imagen TEXT NOT NULL,
 									   contacto TEXT NOT NULL)''')
 
-    def  guardar_tienda(self, tienda):
-        """Recibe un objeto de la clase Tienda y permite insertar sus datos en la tabla tiendas,
-        para esto crea la lista datos_tienda, en la cual se introducen los datos de la tienda,
-        para luego enviarlos a la sentencia que inserta los datos en la base de datos.
-        """
+    def guardar_tienda(self, tienda):
+        """Recibe un objeto de la clase Tienda y permite insertar sus datos en la tabla tiendas."""
 
         datos_tienda = [tienda.nombre_tienda, tienda.direccion_tienda,
 						tienda.categoria, tienda.imagen_portada_tienda,tienda.contacto]
@@ -62,6 +71,50 @@ class Basedatos:
         except sqlite3.IntegrityError as error:
             return (error)
 
+    def devolver_lista_objetos(self,tiendas):
+        """Toma una lista con los resultados de una consulta a
+        la tabla tiendas y devuelve una lista de objetos Tienda"""
+
+        lista_tiendas = []
+        for registro in tiendas:
+                objeto=Tienda(registro[1],registro[2],registro[3],registro[4],registro[5])
+                lista_tiendas.append(objeto)
+        return(lista_tiendas)
+
+    def extraer_tienda(self, id_tienda):
+        """Extrae los datos de la tabla referentes al parámetro id, el cual debe recibir 
+		necesariamente, retorna un objeto de clase Tienda generado a partir de los datos obtenidos, 
+        se puede almacenar en una variable que se debe asignar en la declaración
+        Ej: tienda_recuperada=Basedatos.extraer_tienda(id)"""
+
+        self.conectar_base_datos()
+        self.cursor.execute("SELECT * FROM tiendas WHERE id = ?;", id_tienda)
+        tienda = self.cursor.fetchone()
+        self.cerrar_conexion()
+        lista_datos = tienda[0]
+        tienda_extraida = Tienda(lista_datos[1], lista_datos[2], lista_datos[3],
+                                lista_datos[4], lista_datos[5])
+        return tienda_extraida
+    
+    def extraer_n_tiendas(self,n):
+        """Devuelve una lista con n numero de objetos Tienda, seleccionadas al azar,
+        si el numero de tiendas en la base fuera menor que n se devuelven todas las tiendas"""
+
+        self.conectar_base_datos()
+        self.cursor.execute("SELECT * FROM tiendas ORDER BY random() LIMIT ?;",[n])
+        tiendas = self.cursor.fetchall()
+        self.cerrar_conexion()
+        return self.devolver_lista_objetos(tiendas)
+		
+    def extraer_todas_tiendas(self):
+        """Devuelve una lista de objetos de todas las tiendas almacenadas en la base de datos"""
+
+        self.conectar_base_datos()
+        self.cursor.execute("SELECT * FROM tiendas")
+        tiendas = self.cursor.fetchall()
+        self.cerrar_conexion()
+        return self.devolver_lista_objetos(tiendas)  
+
     def borrar_tienda(self, id_tienda):
         """Borra los datos almacenados en la base de datos, correspondientes al id de la tienda,
         necesariamente se debe pasar el parámetro requerido id de la tienda a borrar"""
@@ -74,53 +127,8 @@ class Basedatos:
         except sqlite3.IntegrityError as error:
             return (error)
 
-    def commit(self):
-        """El método commit valida las transacciones
-        realizadas en la base de datos, se ejecuta luego de realizar transacciones que
-        modifiquen la base de datos"""
-
-        self.db.commit()
-
-    def cerrar_conexion(self):
-        """Cierra el cursor y la conexión a la base de datos"""
-
-        self.cursor.close()
-        self.db.close()
-
-    def extraer_tienda(self, id_tienda):
-        """Extrae los datos de la tabla referentes al parámetro id, el cual debe recibir 
-		necesariamente, retorna un objeto de clase Tienda generado a partir de los datos obtenidos
-        desde la base de datos, se puede almacenar en una variable que se debe asignar en la declaración
-        Ej: tienda_recuperada=Basedatos.extraer_tienda(id)"""
-
-        self.conectar_base_datos()
-        self.cursor.execute("SELECT * FROM tiendas WHERE id = ?;", id_tienda)
-        tienda = self.cursor.fetchone()
-        self.cerrar_conexion()
-        lista_datos = tienda[0]
-        tienda_extraida = Tienda(lista_datos[1], lista_datos[2], lista_datos[3],
-                                lista_datos[4], lista_datos[5])
-        return tienda_extraida
-
-    def todas_las_tiendas(self):
-        """Devuelve una lista de objetos de todas las tiendas almacenadas en la base de datos"""
-
-        self.conectar_base_datos()
-        self.cursor.execute("SELECT * FROM tiendas")
-        tiendas = self.cursor.fetchall()
-        self.cerrar_conexion()
-        lista_tiendas = []
-
-        for registro in tiendas:
-            objeto=Tienda(registro[1],registro[2],registro[3],registro[4],registro[5])
-            lista_tiendas.append(objeto)
-			
-        return lista_tiendas
-
     def __str__(self):
-        """string del objeto"""
-
-        return ("Objeto de clase Basedatos")
+        """Objeto de clase Basedatos"""
 
     def __del__(self):
         """Elimina el objeto """
